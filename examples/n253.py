c61dbb113a4646fc1ca594c8d126076a702b0a55
Peter Teuben <teuben@astro.umd.edu>
2013-05-16 13:33:48 +0000

    align with the talk yesterday

--- examples/n253.py
+++ examples/n253.py
@@ -1,7 +1,10 @@
 #
 # casa script, with some MIRIAD and NEMO example calls
+# within casapy, you can simply execute this script
+# it would be useful to find out if there is some "import casa" style,
+# so you don't have to run this from within casapy?
 #
-# needed at the extended and compact cubes from
+# To grab the original data, get the extended and compact cubes from
 # http://www.astro.umd.edu/~bolatto/NGC253/cubes/compact/newrelease/lines/individual_spws/
 # http://www.astro.umd.edu/~bolatto/NGC253/cubes/extended/newrelease/lines/individual_spws/
 #
@@ -10,7 +13,6 @@
 # with imhead                                        21.526u  7.193s 0:44.55 64.4%   
 # with nemo fitsccd                                  39.267u 12.747s 1:15.89 68.5%   
 # all                                               416.134u 16.151s 7:27.07 96.6% 
-
 
 
 #   from astute
@@ -23,7 +25,6 @@ spw   = range(4)
 
 a = astute.Astute()
 a.need(['NEMO','MIRIAD'])
-
 
 
 once = False               # for testing
@@ -41,7 +42,7 @@ for c in conf:             # loop over configurations and spectral_windows
         if a.has('NEMO'):
             nname = "%s/nemo" % out
             a.nemo('fitsccd in=%s.fits out=%s' % (name,nname))
-            a.nemo('ccdstat %s bad=0 robust=t planes=0 > %s.tab' % (nname,nname))
-        if a.has('MIRIADA'):
-            cmd = 'fits in=%s.fits out=%s/mir op=xyin' % (name,out)
-            a.miriad(cmd)
+            a.nemo('ccdstat %s bad=0 robust=t planes=0 > %s/ccdstat.tab' % (nname,name))
+        if a.has('MIRIAD'):
+            a.miriad('fits in=%s.fits out=%s/mir op=xyin' % (name,out))
+            a.miriad('imstat in=%s/mir log=%s/imstat.tab' % (name,name))
