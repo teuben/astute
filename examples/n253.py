ae8af85d678a15ed29731e4492c552a13d8f7ca1
Peter Teuben <teuben@astro.umd.edu>
2013-05-24 02:31:46 +0000

    cleanup names

--- examples/n253.py
+++ examples/n253.py
@@ -1,7 +1,7 @@
 #
 # casa script, with some MIRIAD and NEMO example calls
 # To run this:
-#     casapy --nogui -c n253.py
+#     casapy --nogui -c $ASTUTE/examples/n253.py
 #
 # it would be useful to find out if there is some "import casa" style,
 # so you don't have to run this from within casapy?
@@ -16,32 +16,35 @@
 # with nemo fitsccd                                  39.267u 12.747s 1:15.89 68.5%   
 # all                                               416.134u 16.151s 7:27.07 96.6% 
 
+#  central box to pick 274,276, 373,350
 
 #   from astute
 import astute
 
 tin  = 'ngc253_fullcube_%s_SPW_%d_contsub_clean'      # the long name
 tout = 'w_%d_%s'                                      # i'm lazy, make a short name
-conf  = ['extended','compact']
-spw   = range(4)
+conf  = ['extended','compact']                        # two configurations
+spw   = [0,1,2,3]                                     # 4 spectral windows
 
 a = astute.Astute()
 a.need(['NEMO','MIRIAD'])                             # this script needs NEMO & MIRIAD
 
 for c in conf:             # loop over configurations and spectral_windows
     for s in spw:
-        name = tin % (c,s)
-        out  = tout % (s,c[0])
-        print " ====== Working on ",name," converting to ",out," ======"
-        importfits(name + ".fits", out, overwrite=True)
-        h = imhead(out,mode='list')
+        iname = tin % (c,s)
+        oname = tout % (s,c[0])
+        
+        # Q?  should mkdir 'oname' and put IM in oname/IM ?
+        print " ====== Working on ",iname," converting to ",oname," ======"
+        importfits(iname + ".fits", oname, overwrite=True)
+        h = imhead(oname,mode='list')
         naxis = h['shape']
         naxis3 = naxis[2]
         print naxis3
         if a.has('NEMO'):
-            nname = "%s/nemo" % out
-            a.nemo('fitsccd in=%s.fits out=%s' % (name,nname))
-            a.nemo('ccdstat %s bad=0 robust=t planes=0 > %s/ccdstat.tab' % (nname,name))
+            a.nemo('fitsccd in=%s.fits out=%s/nemo' % (iname,oname))
+            a.nemo('ccdstat %s/nemo bad=0 robust=t planes=0 > %s/ccdstat.tab' % (oname,oname))
         if a.has('MIRIAD'):
-            a.miriad('fits in=%s.fits out=%s/mir op=xyin' % (name,out))
-            a.miriad('imstat in=%s/mir log=%s/imstat.tab' % (name,name))
+            a.miriad('fits in=%s.fits out=%s/mir op=xyin' % (iname,oname))
+            a.miriad('velsw in=%s/mir axis=radio' % (oname))
+            a.miriad('imstat in=%s/mir log=%s/imstat.tab' % (oname,oname))
