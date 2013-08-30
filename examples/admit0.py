c457e904422cfad094974cc336b1089dd43946c9
Peter Teuben <teuben@astro.umd.edu>
2013-08-30 04:26:37 +0000

    simple

---
+++ examples/admit0.py
@@ -0,0 +1,60 @@
+#
+# admit tinkertoy
+#
+
+
+class Project(object):
+    def __init__(self,name):
+        self.name = name
+        self.s = {}
+
+class Source(object):
+    def __init__(self,name):
+        self.name = name
+        self.b = {}
+        self.l = {}
+
+class Band(object):
+    def __init__(self,name):
+        self.name = name
+
+class Line(object):
+    def __init__(self,name,freq,p=1.0):
+        self.name = name
+        self.freq = freq
+
+
+p1 = Project('p1')
+s1 = Source('s1')
+s2 = Source('s2')
+s3 = Source('s3')
+b1 = Band('1')
+b2 = Band('2')
+b3 = Band('3')
+b4 = Band('4')
+l1 = Line('co',115.27)
+l2 = Line('hcn',88.21)
+
+p1.s[s1.name] = s1
+p1.s[s2.name] = s2
+p1.s[s3.name] = s3
+
+s1.b[b1.name] = b1
+s1.b[b2.name] = b2
+s1.b[b3.name] = b3
+s1.b[b4.name] = b4
+
+s1.l[l1.name] = l1
+s1.l[l2.name] = l2
+
+ns = len(p1.s)
+nb = len(s1.b)
+nl = len(s1.l)
+
+print p1.name,' has ', len(p1.s), ' sources:'
+for s in p1.s.keys():
+    print s
+# notice both return the same number
+print 's1 #bands:', len(s1.b)
+print 's1 #bands:', len(p1.s['s1'].b)
+
