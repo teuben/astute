1d7d1a3e3bf9ac35a5ddaf3995107b641b06d194
Peter Teuben <teuben@astro.umd.edu>
2013-08-30 16:21:11 +0000

    longer names

--- examples/admit0.py
+++ examples/admit0.py
@@ -6,13 +6,13 @@
 class Project(object):
     def __init__(self,name):
         self.name = name
-        self.s = {}
+        self.source = {}
 
 class Source(object):
     def __init__(self,name):
         self.name = name
-        self.b = {}
-        self.l = {}
+        self.band = {}
+        self.line = {}
 
 class Band(object):
     def __init__(self,name):
@@ -35,26 +35,27 @@ b4 = Band('4')
 l1 = Line('co',115.27)
 l2 = Line('hcn',88.21)
 
-p1.s[s1.name] = s1
-p1.s[s2.name] = s2
-p1.s[s3.name] = s3
+p1.source[s1.name] = s1  # deeper
+p1.source[s2.name] = s2
+p1.source[s3.name] = s3
 
-s1.b[b1.name] = b1
-s1.b[b2.name] = b2
-s1.b[b3.name] = b3
-s1.b[b4.name] = b4
+s1.band[b1.name] = b1  # deeper
+s1.band[b2.name] = b2
+s1.band[b3.name] = b3
+s1.band[b4.name] = b4
 
-s1.l[l1.name] = l1
-s1.l[l2.name] = l2
+s1.line[l1.name] = l1  # deeper
+s1.line[l2.name] = l2
 
-ns = len(p1.s)
-nb = len(s1.b)
-nl = len(s1.l)
+ns = len(p1.source)
+nb = len(s1.band)
+nl = len(s1.line)
 
-print p1.name,' has ', len(p1.s), ' sources:'
-for s in p1.s.keys():
+print p1.name,' has ', len(p1.source), ' sources:'
+for s in p1.source.keys():
     print s
 # notice both return the same number
-print 's1 #bands:', len(s1.b)
-print 's1 #bands:', len(p1.s['s1'].b)
+print 's1 #bands:', len(s1.band)
+for s in p1.source.keys():
+    print '%s #bands:%d' % ( s , len(p1.source[s].band))
 
