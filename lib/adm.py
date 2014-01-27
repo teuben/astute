e481b9acfd84d2696f9ee3c1142aaee1e00e9892
Peter Teuben <teuben@astro.umd.edu>
2014-01-27 23:33:24 +0000

    template

---
+++ lib/adm.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+#
+#  looping over directories
+
+import numpy             as np
+import matplotlib.pylab  as plt
+#
+from scipy.interpolate  import spline
+import pylab
+
+
+__version__ = "pre-ADMIT functions: $Id$"
+
+
+
+
+if __name__ == '__main__':
+    print "No __main__ yet, if ever...."
