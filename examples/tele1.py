48ded8d81e526d738f0e6ccc5ab82a4a1fd9f234
Peter Teuben <teuben@astro.umd.edu>
2014-06-23 12:59:32 +0000

    sma

--- examples/tele1.py
+++ examples/tele1.py
@@ -4,6 +4,8 @@
 # ALMA:    project, sous, gous, mous
 # CARMA:   project, obsblock, subobsblock, trial
 # BIMA:
+# SMA:     year+semester, project#,       (e.g. 2011B-S048 
+#          /rtdc/download-2011/120318_053300_m83_off/RAW/120318_053300.tar.gz 
 # GENERIC: project
 
 class tel(object):
