bd9bdfd89011ce51d3a88c5183e2c59aa02a3897
Peter Teuben <teuben@astro.umd.edu>
2014-08-29 16:58:09 +0000

    various flow; writing of BDP's now under control of a dependancy

--- lib/admit1.py
+++ lib/admit1.py
@@ -18,6 +18,8 @@ _debug = True
 nprojects = 100
 nlines    = 100
 
+# ==============================================================================
+
 class ADMIT(object):
     def __init__(self, name='none'):
         self.name    = name
@@ -29,8 +31,12 @@ class ADMIT(object):
     def add(self, b):
         """Add a BDP to the stack of BDP's this ADMIT pipeline contains"""
         # note there needs to be a cleanup/delete/pop option here
-        b1 = copy.deepcopy(b)
-        self.bdps.append(b1)
+        # __main__ : deepcopy ga ve 19780 bytes,  re-assign + deepcopy was 22973 bytes, why ?
+        if False:
+            b1 = copy.deepcopy(b)
+            self.bdps.append(b1)
+        else:
+            self.bdps.append(b)
     def set(self,a=None, b=1, c=[]):
         """set a global ADMIT parameter
            The idea is that these are obtained through introspection
@@ -70,24 +76,44 @@ class ADMIT(object):
         else:
             pname = name
         print "ADMIT: pickle saving %s" % pname
+        btmp = self.bdps
+        self.bdps = []
+        for b in btmp:
+            b1 = copy.deepcopy(b)
+            self.bdps.append(b1)
         pickle.dump(self,open(pname,"wb"))
+        del btmp
     def pload(self,pname):
         print "ADMIT: pickle loading %s" % pname
         return pickle.load(open(pname,"rb"))
 
+# ==============================================================================
 
 class BDP(object):
+    """ Base class for BDP
+        should have some static members
+    """
+    #
     def __init__(self, name='none',filename=None, filetype=None):
         self.name     = name
+        self.id       = 0
         self.filename = filename
         self.filetype = filetype
         self.data     = {}
         #
-        self.updated  = False
+        self.updated  = False     # ???   this triggers a new run
+        self.output   = True      # True: triggers a new save(pdump/xwrite)
         self.deps     = []
         self.derv     = []
         self.task     = []
         if _debug: print "BDP(%s,%s,%s) " % (name,filename,filetype)
+    def parse(self,doParse = True):
+        """ Doug's XML parser, automatically invoked if
+            you do b = BDP_foobar("xmlfile")
+        """
+    def write(self,xmlFile = None):
+        """ Doug's XML writer
+        """
     def show(self):
         return self.filename
     def update(self,new_state):
@@ -111,8 +137,14 @@ class BDP(object):
             print "BDP::task %s" % t.show()
     def set(self,keyval):
         if _debug: print "BDP::set %s" % keyval
+    def pdump(self,filename):
+        """ save, if needed """
+        if self.output:
+            if _debug: print "BDP.pdump(%s)" % filename
+            pickle.dump(self,open(filename,"wb"))
+            self.output = False
     def run(self):
-        if self.updated: 
+        if self.updated:
             if _debug: print "BDP::%s (%s) was up to date" % (self.name,self.filename)
             return False
         if _debug: print "BDP::%s (%s) running" % (self.name,self.filename)
@@ -130,6 +162,9 @@ class BDP_buckett(BDP):
     This BDP is a random collection of things you find useful. Less rigurous testing
     and verfication on valid data is done here, and gets you going quickly for
     using is.
+    Use the dictionary member data{} to place anything you like
+    e.g.   b.data['table1'] = my_atable
+    This pickles ok, but must be a nightmare for XML. 
     """
     def __init__(self, filename=None, filetype=None):
         BDP.__init__(self,"BUCKETT",filename,filetype)
@@ -137,14 +172,14 @@ class BDP_buckett(BDP):
 
 class BDP_file(BDP):
     """
-    Contains a reference to a file
+    Contains a reference to a dataset (fits file, casa image, ...)
     """
     def __init__(self, filename=None, filetype=None):
         BDP.__init__(self,"FILE", filename, filetype)
 
 class BDP_table(BDP):
     """
-    Contains a reference to a file
+    Contains a reference to a table (shouldn't BDP_file suffice?)
     """
     def __init__(self, filename=None, filetype=None):
         BDP.__init__(self,"TABLE", filename, filetype)
@@ -152,7 +187,7 @@ class BDP_table(BDP):
 
 class BDP_cubestats(BDP):
     """
-    Contains a reference to a file
+    Contains a reference to a cubestats table
     """
     def __init__(self, filename=None, filetype=None):
         BDP.__init__(self,"CUBESTATS", filename, filetype)
@@ -164,7 +199,7 @@ class BDP_cubestats(BDP):
 
 class BDP_cubespectrum(BDP):
     """
-    Contains a reference to a file
+    Contains a reference to a cubespectrum table
     """
     def __init__(self, filename=None, filetype=None):
         BDP.__init__(self,"CUBESPECTRUM", filename, filetype)
@@ -174,7 +209,12 @@ class BDP_cubespectrum(BDP):
     def get_table(self):
         return self.table
 
+
+# ==============================================================================
+
 class AT(object):
+    # not really static accross instantiations
+    env     = 'casa'
     name    = 'generic'
     version = '1.0'
     keys    = []
@@ -191,6 +231,11 @@ class AT(object):
             for b1 in bdp_in:
                 if b1 != b2:
                     b2.depends_on(b1)
+                    b1.output = True
+                else:
+                    # formally, b1 should never be b2
+                    print "bad bad bad, b1==b2"
+                    
     def show(self):
         return self.name
     def run(self):
@@ -207,7 +252,14 @@ class AT(object):
             return False
         for b2 in self.bdp_out:
             b2.updated = True
+            b2.output  = True
         return True
+    def setenv(self,newval):
+        # playing with env
+        self.env = newval
+    def getenv(self):
+        # why is env not available
+        return self.env
     def set(self,keyvals):
         kv = keyvals.split('=')
         self.setkv({ kv[0] : kv[1] })
@@ -234,12 +286,22 @@ class AT(object):
             print "  BDP_OUT:"
             for b in self.bdp_out:  print "    ",b.show()
     def pdump(self, ext='p'):
+        """  pickle dumps all the output BDP's
+             @todo   parents need to be signalled if they have new children
+                     so they need to save their state if BDP's are saved
+                     after every .run
+                     
+        """
         for b in self.bdp_out:
             pname = b.filename + "." + ext
-            print "writing %s" % pname
-            pickle.dump(b,open(pname,"wb"))
+            b.pdump(pname)
+        # also check the inputs, since they may <have new dependencies
+        for b in self.bdp_in:
+            pname = b.filename + "." + ext
+            b.pdump(pname)
+
     def pload(self, ext='p', filename=None):
-        """ is this sane? """
+        """ is this even sane? """
         if filename:
             self.bdp_in[0] = pickle.load(open(filename,"rb"))
             return self.bdp_in[0]
@@ -266,7 +328,10 @@ class AT_simple(AT):
         #     ... contain parameters
 
 class AT_file(AT):
-    """ Create a simple container for a file"""
+    """ Create a simple container for a file
+        bdp_in:   none
+        bdp_out:  one file
+    """
     name    = 'FILE'
     version = '1.0'
     keys    = ['type']
@@ -379,24 +444,14 @@ class AT_cubespectrum(AT):
         if self.do_plot:
             a1.plotter(freq,[data],'CubeSpectrum',filename+'.png')
 
-class AT_combine(AT):
-    """ combine two BDPs into one"""
-    name = 'COMBINE'
-    version = '1.0'
-    keys = []
-    def __init__(self,bdp_in=[],bdp_out=[]):
-        AT.__init__(self,self.name,bdp_in,bdp_out)
-        if _debug: print "AT_combine.init"
-    def run(self):
-        if _debug: print "AT_combine.run"
-        if not AT.run(self):
-            return False
-        # specialized work can commence here
-        print "  work: %d -> %d" % (len(self.bdp_in),len(self.bdp_out))
-        print "  in:  ",self.bdp_in[0].show(),self.bdp_in[1].show()
-        print "  out: ",self.bdp_out[0].show()
 
-
+# the classes below are for playing with pipelines, they do no real work.
+#
+# flow:   1 -> 1
+# flow12: 1 -> 2
+# flow21: 2 -> 1
+# flowN:  1 -> N 
+#
 
 class AT_flow(AT):
     """ change one BDP into another one"""
@@ -426,10 +481,10 @@ class AT_flow(AT):
         """AT.get() retrieved current values"""
         print "AT.get"
 
-class AT_flow2(AT):
+class AT_flow12(AT):
     """ split one BDP into two """
-    name = 'FLOW2'
-    version = '2.0'
+    name = 'FLOW12'
+    version = '1.0'
     keys = []
     def __init__(self,bdp_in=[],bdp_out=[]):
         if _debug: print "AT_flow2.init"
@@ -441,22 +496,63 @@ class AT_flow2(AT):
             return False
         # specialized work can commence here
         print "  work_flow2: %d -> %d" % (len(self.bdp_in),len(self.bdp_out))
+        if self.do_pickle:
+            self.pdump()
+
+class AT_flow21(AT):
+    """ combine two BDPs into one"""
+    name = 'FLOW21'
+    version = '1.0'
+    keys = []
+    def __init__(self,bdp_in=[],bdp_out=[]):
+        AT.__init__(self,self.name,bdp_in,bdp_out)
+        if _debug: print "AT_combine.init"
+    def run(self):
+        if _debug: print "AT_combine.run"
+        if not AT.run(self):
+            return False
+        # specialized work can commence here
+        print "  work: %d -> %d" % (len(self.bdp_in),len(self.bdp_out))
+        print "  in:  ",self.bdp_in[0].show(),self.bdp_in[1].show()
+        print "  out: ",self.bdp_out[0].show()
+        if self.do_pickle:
+            self.pdump()
+
+class AT_flow22(AT):
+    """ arrange two BDPs into two others"""
+    name = 'FLOW22'
+    version = '1.0'
+    keys = []
+    def __init__(self,bdp_in=[],bdp_out=[]):
+        AT.__init__(self,self.name,bdp_in,bdp_out)
+        if _debug: print "AT_combine.init"
+    def run(self):
+        if _debug: print "AT_combine.run"
+        if not AT.run(self):
+            return False
+        # specialized work can commence here
+        print "  work: %d -> %d" % (len(self.bdp_in),len(self.bdp_out))
+        print "  in:  ",self.bdp_in[0].show(),self.bdp_in[1].show()
+        print "  out: ",self.bdp_out[0].show()
+        if self.do_pickle:
+            self.pdump()
 
 class AT_flowN(AT):
     """Split one BDP into N BDPs"""
     name = 'FLOWN'
     version = '1.0'
-    keys = []
+    keys = ['n']
     def __init__(self,bdp_in=[],bdp_out=[]):
         if _debug: print "AT_flowN.init"
         AT.__init__(self,self.name,bdp_in,bdp_out)
-    # note the deliberate bug to not implement the .run() here
     def run(self):
         if _debug: print "AT_flowN.run"
         if not AT.run(self):
             return False
         # specialized work can commence here
         print "  work_flowN: %d -> %d" % (len(self.bdp_in),len(self.bdp_out))
+        if self.do_pickle:
+            self.pdump()
 
 
 # running a series of BDP's is not right if BDP's can have different state
@@ -470,39 +566,51 @@ def pipeline(bdps):
 
 if __name__ == "__main__":
 
-    # pickle files store the module name, so keep that here
+    # pickle files store the module name, so keep that here, 
+    # since that is how we run it externally
     import admit1 as admit
 
-    #  silly stuff to limit the flow of the pipeline
-    Q1 = True
-    #Q1 = False
-    Q2 = False
-    Q2 = True
+    #  silly stuff to limit the flow of the pipeline (Q=0 only produces b0, Q=1 the b1, etc.)
+    Q = 0
+    if len(sys.argv)>1:
+        Q = int(sys.argv[1])
     #
     print "TESTING admit1.py:"
     #
-    a = admit.ADMIT("TRY1a")
-    #                                                     setting the FITS file BDP
-    b0 = admit.BDP_file("test0","fits")
-    a0 = admit.AT_file([],[b0])
-    a0.set('a=1')
-    a0.run()
-    a.add(b0)
-    if Q1:
-        b1 = admit.BDP_buckett("test1","cube")
-        a1 = admit.AT_ingest([b0],[b1])
-        a1.run()
-        a.add(b1)
-        #
-        if Q2:
-            n = 10
-            b2 = range(n)
-            for i in range(n):
-                id = "linecube_%d" % i
-                b2[i] = admit.BDP_buckett(id,"cube")
-            a2 = admit.AT_flowN([b1],b2)
-            a2.run()
-            for i in range(n):
-                a.add(b2[i])
-    a0.pdump('test00.p')
+    a = admit.ADMIT()
+    if Q>=0:
+        b0 = admit.BDP_file("b0")
+        a0 = admit.AT_file([],[b0])
+        a0.run()
+        a.add(b0)
+        if Q>0:
+            b1 = admit.BDP_buckett("b1")
+            a1 = admit.AT_flow([b0],[b1])
+            a1.run()
+            a.add(b1)
+            if Q>1:
+                b21 = admit.BDP_buckett("b21")
+                b22 = admit.BDP_buckett("b22")
+                a2 = admit.AT_flow12([b1],[b21,b22])
+                a2.run()
+                a.add(b21)
+                a.add(b22)
+                if Q>2:
+                    b3 = admit.BDP_buckett("b3")
+                    a3 = admit.AT_flow21([b21,b22],[b3])
+                    a3.run()
+                    a.add(b3)
+                    if Q>3:
+                        n = 10
+                        b4 = range(n)
+                        for i in range(n):
+                            id = "b4_%d" % i
+                            b4[i] = admit.BDP_buckett(id)
+                        a4 = admit.AT_flowN([b3],b4)
+                        a4.run()
+                        for i in range(n):
+                            a.add(b4[i])
+    # a0.pdump('test00.p')
     a.pdump('admit1.p')
+    b0.pdump('b00.p')
+    print "Q=%d" % Q
