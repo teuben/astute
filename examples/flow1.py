0ea94f9f4cae2e4816933e3b19a2739853f3a270
Peter Teuben <teuben@astro.umd.edu>
2014-09-04 16:38:28 +0000

    docs for this and the other flow's

--- examples/flow1.py
+++ examples/flow1.py
@@ -1,7 +1,15 @@
 #! /usr/bin/env python
 #
-#  Simple data flow using flow (flow11)
+#  Simple data flow using flow (one in, one out)
 #
+#  In this and subsequent example scripts, various types
+#  of ADMIT flow patterns are investigated for correct
+#  behavior.
+#  1) are dependencies correct for complex flow, does
+#     reexecution behave correctly.
+#  2) are parameters stored in the correct place
+#  3) does XML save and restore state 
+#  4) does Pickly do the same
 
 import admit1 as admit
 
