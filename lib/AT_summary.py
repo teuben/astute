827a511a62636609910beca4bd4274cbcfbb8ba2
Peter Teuben <teuben@astro.umd.edu>
2014-09-16 21:59:11 +0000

    verbose

--- lib/AT_summary.py
+++ lib/AT_summary.py
@@ -8,7 +8,7 @@ import admit1 as admit
 class AT_summary(admit.AT):
     name = 'SUMMARY'
     version = '1.0'
-    keys = ['']
+    keys = ['verbose']
     def __init__(self,bdp_in=[],bdp_out=[]):
         admit.AT.__init__(self,self.name,bdp_in,bdp_out)
     def run(self):
@@ -18,14 +18,21 @@ class AT_summary(admit.AT):
         fni = self.bdp_in[0].filename
         fno = self.bdp_out[0].filename
         #
+        #
+        verbose = self.geti('verbose',0)
+        #
         taskinit.ia.open(fni)
         s = taskinit.ia.summary()
-        print s['axisnames']
-        print s['axisunits']
-        print s['shape']
-        print s['incr']
-        print s['restoringbeam']
-        print s['refval']
+        if verbose:
+            print s
+        print '============================================================================'
+        print 'AXIS',s['axisnames']
+        print 'UNIT',s['axisunits']
+        print 'SHAP',s['shape']
+        print 'INCR',s['incr']
+        print 'BMAJ',s['restoringbeam']['major']
+        print 'COOR',s['refval']
+        print '============================================================================'
         # just save a few for now, should be a formal container, not data{}
         for i in ['shape', 'refval', 'incr']:
             self.bdp_out[0].data[i] = s[i]
