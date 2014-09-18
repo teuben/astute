de169b575228c59d85b4cb2b3d0b297d2c6574c3
Peter Teuben <teuben@astro.umd.edu>
2014-09-18 02:49:52 +0000

    add mask

--- lib/AT_ingest.py
+++ lib/AT_ingest.py
@@ -15,15 +15,17 @@ class AT_ingest(admit.AT):
         if not admit.AT.run(self):
             return False
         # specialized work can commence here
-        create_mask = True
+        create_mask = self.getb('mask',0)
         fni = self.bdp_in[0].filename
         fno = self.bdp_out[0].filename
         print "casa::ia.fromfits(%s)" % fni
         taskinit.ia.fromfits(fno,fni,overwrite=True)
         if create_mask:
+            # one of the egnog cubes has 1 bad plane
+            # but this fails on n253 cubes (nan on the statistics)
             print "casa::ia.calcmask"
             taskinit.ia.calcmask('"%s" != 0.0' % fno)
         s = taskinit.ia.summary()
         s0 = taskinit.ia.statistics()
-        print 'SHAPE: ', s['shape'],s0['npts']
+        print 'BASICS: ', s['shape'],s0['npts'],s0['min'],s0['max']
         taskinit.ia.close()
