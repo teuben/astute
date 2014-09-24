076a5f9723fcacd598c2501d2c289f20ba22c18d
Peter Teuben <teuben@astro.umd.edu>
2014-09-24 19:16:24 +0000

    shortened using integer task index

--- examples/flow2-at.py
+++ examples/flow2-at.py
@@ -10,12 +10,13 @@ import admit2 as admit
 a = admit.ADMIT('flow2')
 
 
-a1 = admit.AT_file()      ; a.add(a1)
-a1.set("file=foobar.fits")
-a1.run()                                    # a1[0] is now the BDP (b0)
+a1 = admit.AT_file('name=flow2')   
+i1 = a.add(a1)
+a1.run()
+
+a2 = admit.AT_flow12()
+i2 = a.add(a2, [(i1,0)])
+a2.run()
 
 
-a2  = admit.AT_flow12([ a1[0] ]) ; a.add(a2)
-a2.run()                                    # a2[0] and a2[1] are now the BDP's (b11,b12)
-
-
+a.show()
--- examples/flow4-at.py
+++ examples/flow4-at.py
@@ -9,20 +9,38 @@
 import sys, os
 import admit2 as admit
 
+#   import admit.AT_casa as AT
+#   a1 = AT.ingest('foobar.fits')
+
+
+
 a = admit.ADMIT()
 
+if False:
+    a1 = admit.AT_file("flow4")
+    i1 = a.add(a1)
+    a1.run()
 
-a1 = admit.AT_file()      ; a.add(a1)
-a1.set("file=foobar.fits")
-a1.run()                                    # a1[0] is now the BDP (b0)
+    a2 = admit.AT_flow()
+    i2 = a.add(a2, [(i1,0)] )
+    a2.run()
 
+    a3 = admit.AT_flow()
+    i3 = a.add(a3, [(i1,0)] )
+    a3.run()
 
-a2 = admit.AT_flow([a1[0]]) ; a.add(a2)
-a2.run()                                    # a2[0] is the BDP out (b1)
+    a4 = admit.AT_flow21()
+    i4 = a.add(a4, [(i2,0),(i3,0)])
+    a4.run()
+else:
+    i1 = a.add(admit.AT_file("flow4"))
+    i2 = a.add(admit.AT_flow(),             [(i1,0)] )
+    i3 = a.add(admit.AT_flow(),             [(i1,0)] )
+    i4 = a.add(admit.AT_flow21(),           [(i2,0),(i3,0)])
+    a[i4].set('a=1')
 
-a3 = admit.AT_flow([a1[0]]) ; a.add(a3)
-a3.run()                                    # a3[0] is the BDP out (b2)
+    a.run()
 
-a4 = admit.AT_flow21([a2[0],a3[0]]) ; a.add(a4)
-a4.run()                                    # a4[0] is the BDP out (b3)
+print i1,i2,i3,i4
 
+a.show()
