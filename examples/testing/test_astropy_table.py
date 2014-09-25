85400701e6984bcd2bab8d88d9d3fb0a27968d5a
Peter Teuben <teuben@astro.umd.edu>
2014-09-25 12:35:49 +0000

    upd

--- examples/testing/test_astropy_table.py
+++ examples/testing/test_astropy_table.py
@@ -1,14 +1,30 @@
 #! /usr/bin/env python
 #
 
-import os
+import os, sys
+
+
+try:
+    import astropy
+    print "astropy OK"
+except:
+    print "no astropy"
+    sys.exit(0)
 
 try:
     from astropy.table import Table
 except:
-    print "no astropy"
-    os.exit(0)
- 
-print "Testing astropy.table"
+    print "astropy.table MISSING"
+    sys.exit(0)
+
+
+fno =  'test_astropy_table.tab'
+print "Testing astropy.table",fno
 
  
+a = [1, 4, 5]
+b = [2.0, 5.0, 8.2]
+c = ['x', 'y', 'z']
+t = Table([a, b, c], names=('a', 'b', 'c'), meta={'name': 'first table'})
+print t
+t.write(fno,format='ascii.commented_header')
