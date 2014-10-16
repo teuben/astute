97afea78e6fde7e333cb5611487ded36ce91a6c8
Peter Teuben <teuben@astro.umd.edu>
2014-10-16 14:05:06 +0000

    symlink option

--- lib/AT2/AT_ingest.py
+++ lib/AT2/AT_ingest.py
@@ -1,14 +1,22 @@
 import sys, os
 import atable
 import numpy as np
-#import casa
-import taskinit
+import taskinit, casa
 import admit2 as admit
 
 class AT_ingest(admit.AT):
+    """
+    ingest an image (usually fits) into casa [in this case]
+    - can keep this a symlink, because CASA can read FITS files
+    - miriad is also allowed if the symlink is working
+    CAVEAT: symlink may not always work
+    
+    todo:  -allow casa region. this cannot be used in symlink mode
+           -allow flux map (pbcorr) correction
+    """
     name = 'INGEST'
     version = '1.0'
-    keys = ['mask','file','skip']
+    keys = ['mask','file','skip','symlink','region','pbcorr']
     def __init__(self,name=None):
         if name != None: self.name = name
         admit.AT.__init__(self,self.name)
@@ -18,11 +26,19 @@ class AT_ingest(admit.AT):
         if not admit.AT.run(self):
             return False
         # specialized work can commence here
-        skip = self.geti('skip',0)
+        skip = self.getb('skip',0)
+        if skip: return
+        symlink = self.getb('symlink',0)
+        if symlink:
+            # should we test the status of fno ?
+            cmd = 'ln -sf %s %s' % (fni,fno)
+            print "INGEST: ",cmd
+            os.system(cmd)
+            return
+
         create_mask = self.getb('mask',0)
         fni = self.get('file')
         fno = self.bdp_out[0].filename
-        if skip: return
 
         print "casa::ia.fromfits(%s)" % fni
         taskinit.ia.fromfits(fno,fni,overwrite=True)
